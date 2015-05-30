0ba49de84cdd6beb52b91ca0c87b708f4dc5892e
sntq4sk <sntq4sk@naver.com>
2015-05-30 12:51:16 +0900

    가입

---
+++ register/database/tobby-data.sql
@@ -0,0 +1,29 @@
+insert into tendency
+values ("a1", "독고다이형");
+
+insert into tendency
+values ("a2", "토론형");
+
+insert into tendency
+values ("a3", "실습형");
+
+insert into tendency
+values ("b1", "텀프로젝트");
+
+insert into tendency
+values ("b2", "팀플레이");
+
+insert into tendency
+values ("b3", "개인과제");
+
+insert into tendency
+values ("c1", "서술형");
+
+insert into tendency
+values ("c2", " 혼합형");
+
+insert into tendency
+values ("c3", "오픈북");
+
+insert into tendency
+values ("c4", "실습");
