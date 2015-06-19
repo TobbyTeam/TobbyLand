855354d80dcc80de396b02ba3b56b0f981b2edf2
babsagn24 <babsang24@hanmail.net>
2015-06-19 12:40:58 +0900

    member 커멘드 객체

---
+++ src/main/java/kr/ac/kpu/ebiz/spring/tobbyproject/member/Member.java
@@ -0,0 +1,71 @@
+package kr.ac.kpu.ebiz.spring.tobbyproject.member;
+
+/**
+ * Created by happy on 2015-06-19.
+ */
+public class Member {
+
+    private String member_id;
+    private String password;
+    private String nickname;
+    private String email;
+    private String method;
+    private String task;
+    private String exam;
+
+    public String getMember_id() {
+        return member_id;
+    }
+
+    public void setMember_id(String member_id) {
+        this.member_id = member_id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getExam() {
+        return exam;
+    }
+
+    public void setExam(String exam) {
+        this.exam = exam;
+    }
+}
